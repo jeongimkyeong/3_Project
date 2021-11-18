67c5145f0b9d0760f7b0fb07461aa5436b3c6054
Administrator <Administrator@DESKTOP-CNRM838>
2021-11-19 02:47:56 +0900

    cursor:pointer 추가 LoginJoin.css 에서 memberDB.sql 에서 board_tbl 생성 쿼리문 추가

--- src/main/webapp/memberDB.sql
+++ src/main/webapp/memberDB.sql
@@ -16,3 +16,21 @@ create table member_tbl(
 );
 
 desc member_tbl;
+
+create table board_tbl(
+	num int primary key auto_increment,
+	userid varchar(45) not null,
+	username varchar(45) not null,
+	subject varchar(20) not null,
+	content text not null,
+	regdate date not null,
+	pwd varchar(15),
+	count int unsigned,
+	filename varchar(30),
+	filesize int,
+    foreign key(userid) references member_tbl(userid)
+    on update cascade
+    on delete cascade
+);
+
+/*전체 실행 : Ctrl+Alt+X , 드래그한부분 실행 : Alt+X*/
