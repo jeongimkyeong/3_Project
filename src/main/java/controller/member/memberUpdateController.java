6ed2c55d36a2a4fbd20e9a23ce2f5779c0c4d1a6
Administrator <Administrator@DESKTOP-CNRM838>
2021-11-23 21:56:25 +0900

    /module 폴더안으로 Join폴더와 Login폴더 이동 Header.jsp 에 로그인,회원가입 링크 변경 Join폴더와 Login폴더 안에 경로도 변경 LoginHeader.jsp 에 정보수정 링크경로 삽입 FrontController에 회원수정 memberUpdateController 추가 회원탈퇴 memberDeleteController 추가

---
+++ src/main/java/controller/member/memberUpdateController.java
@@ -0,0 +1,15 @@
+package controller.member;
+
+import controller.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class memberUpdateController implements Controller{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
